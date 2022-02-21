763d881544a7c1e9745c8b267cb2e3120700b788
josejr16 <100150238+josejr16@users.noreply.github.com>
2022-02-21 11:05:54 -0500

    Add files via upload

---
+++ tarea3.py
@@ -0,0 +1,9 @@
+num_1 = float(input("ingrese el numero"))
+num_2 = float(input("ingrese el numero"))
+if num_1 == num_2:
+    print("son iguales")
+elif num_1 == num_2:
+    print("el mayor es ", num_1)
+
+elif num_1 == num_2:
+    print("el mayor es ", num_1")cd
